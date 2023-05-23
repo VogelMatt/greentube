12a38d6c1c545891b6db55dbe6f0fb93dfb1348e
ToastyandTasty <mahvogel@gmail.com>
2023-05-22 23:45:58 -0500

    add app dependencies

--- src/App.js
+++ src/App.js
@@ -1,9 +1,12 @@
 import React from 'react'
 
 const App = () => {
-  return (
-    <div>App</div>
-  )
+  return <>
+  <div>App</div>
+  <div>hello world</div>
+  
+  </>
+  
 }
 
 export default App
