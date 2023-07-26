8aafbe100ef8566f78429878e2bb45e75c539374
ToastyandTasty <mahvogel@gmail.com>
2023-07-25 23:58:50 -0500

    mui added

--- src/App.js
+++ src/App.js
@@ -1,12 +1,23 @@
 import React from 'react'
+import { BrowserRouter, Routes, Route } from 'react-router-dom'
+import { Box } from '@mui/material'
+import { Feed } from '@mui/icons-material'
 
-const App = () => {
-  return <>
-  <div>First world order</div>
-  <div>GreenTube</div>
-  
-  </>
-  
-}
+
+const App = () => (
+  <BrowserRouter>
+  <Box sx={{backgroundColor: '#000'}}>
+    Navbar
+    <Routes>
+      <Route path="/" exact element {<Feed />}/>
+      <Route path="/Video/:id" element={<VideoDetail />} />
+
+      
+      
+    </Routes>
+  </Box>
+  </BrowserRouter>
+
+);
 
 export default App
