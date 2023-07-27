6d14ca1e11b5f4b167d01f754ccdd541ab3eadc0
ToastyandTasty <mahvogel@gmail.com>
2023-07-26 23:57:43 -0500

    component update

--- src/App.js
+++ src/App.js
@@ -1,16 +1,21 @@
-import React from 'react'
 import { BrowserRouter, Routes, Route } from 'react-router-dom'
 import { Box } from '@mui/material'
+
 import { Feed } from '@mui/icons-material'
+import Navbar from './components/Navbar';
+
+import {Navbar, Feed, VideoDetail, ChannelDetail, SearchFeed} from './components'
 
 
 const App = () => (
   <BrowserRouter>
   <Box sx={{backgroundColor: '#000'}}>
-    Navbar
+    <Navbar/> 
     <Routes>
       <Route path="/" exact element {<Feed />}/>
-      <Route path="/Video/:id" element={<VideoDetail />} />
+      <Route path="/video/:id" element={<VideoDetail />} />
+      <Route path="/channel/:id" element={<ChannelDetail />} />
+      <Route path="/search/:id" element={<SearchFeed />} />
 
       
       
