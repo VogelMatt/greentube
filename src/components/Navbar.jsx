337ca28ba736ac7138b30fa39aaa3b27d3d9b8cb
ToastyandTasty <mahvogel@gmail.com>
2023-08-22 23:52:39 -0500

    constants

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -2,11 +2,14 @@
 import { Stack } from '@mui/material'
 import { Link } from 'react-router-dom'
 
-
-const Navbar = () => {
-  return (
-    <div>Navbar</div>
-  )
-}
-
+import { logo } from '../utils/constants';
+const Navbar = () => (
+  <Stack
+  direction="row"
+  alignItems="center"
+  p={2}
+  sx= {{ postion: 'sticky', background: '#000', top: 0, justifyContent: "space-between"}}>
+  
+  </Stack>
+)
 export default Navbar
