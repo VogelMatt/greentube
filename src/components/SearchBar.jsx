6d285f2ed5f32f99041db7fb925d218801e39811
ToastyandTasty <mahvogel@gmail.com>
2023-08-26 00:00:08 -0500

    search icon

--- src/components/SearchBar.jsx
+++ src/components/SearchBar.jsx
@@ -1,7 +1,7 @@
 import { useState } from 'react'
 import { useNavigate } from 'react-router-dom'
 import { Paper, IconButton } from '@mui/material'
-import { SearchIcon } from '@mui/icons-material'
+import { Search } from '@mui/icons-material'
 
 
 const SearchBar = () => {
@@ -23,7 +23,9 @@ const SearchBar = () => {
         value=""
         onChange={() => {}}
       />  
-      
+      <IconButton>
+        <Search/>
+      </IconButton>
       SearchBar</Paper>
   )
 }
