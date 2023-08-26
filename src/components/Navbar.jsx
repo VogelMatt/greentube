dc501f84468c4f7caa9b53053935e5e2c2dba4e3
ToastyandTasty <mahvogel@gmail.com>
2023-08-25 23:58:05 -0500

    search bar part dos

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -3,6 +3,7 @@ import { Stack } from '@mui/material'
 import { Link } from 'react-router-dom'
 
 import { logo } from '../utils/constants';
+import SearchBar from './SearchBar';
 const Navbar = () => (
   <Stack
   direction="row"
@@ -15,9 +16,7 @@ const Navbar = () => (
 
 
 </Link>
-<SearchBar>
-  
-</SearchBar>
+<SearchBar/>
 
 
   </Stack>
