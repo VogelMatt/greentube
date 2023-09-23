11880b7641932e64b2a4618c2ea7c041e6b5aee6
ToastyandTasty <mahvogel@gmail.com>
2023-09-22 23:46:02 -0500

    api key

--- src/components/Feed.jsx
+++ src/components/Feed.jsx
@@ -2,9 +2,12 @@ import { useState, useEffect } from "react";
 import { Box, Stack, Typography } from "@mui/material";
 
 import { Sidebar , Videos } from "./";
-
+import { fetchFromAPI } from "../utils/fetchFromAPI";
 
 const Feed = () => {
+
+  useEffect()
+
   return (
     <Stack sx={{ flexDirection: { sx: "column", md: "row" } }}>
       <Box sx={{ height: { sx: 'auto', md: '92vh' }, borderRight: '1px solid #3d3d3d', px: {sx: 0, md: 2 } }}>
