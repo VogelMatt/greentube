73581521684371ffad2ab6380623d4ed8131a11c
ToastyandTasty <mahvogel@gmail.com>
2023-10-10 18:12:07 -0500

    api not working

--- src/components/VideoCard.jsx
+++ src/components/VideoCard.jsx
@@ -1,6 +1,9 @@
-import React from 'react'
+import { link } from 'react-router-dom'
+import { Typography, Card, CardContent, CardMedia } from '@mui/material'
+import { CheckCircle } from '@mui/icons-material'
+import { demoThumbnailUrl, demoVideoUrl,demoVideoTitle, demoChannelUrl, demoChannelTitle } from '../utils/constants'
 
-const VideoCard = () => {
+const VideoCard = ({video}) => {
   return (
     <div>VideoCard</div>
   )
--- src/utils/fetchFromAPI.js
+++ src/utils/fetchFromAPI.js
@@ -1,20 +1,30 @@
 import axios from 'axios';
 
-const Base_URL= 'https://youtube-v31.p.rapidapi.com/search';
+const Base_URL= 'https://youtube-v3-lite.p.rapidapi.com/search';
+
+// const options = {
+//     params: {    
+        
+//   },
+//   headers: {
+//     'X-RapidAPI-Key': 
+//     'X-RapidAPI-Host': 'youtube-v31.p.rapidapi.com'
+//   }
+// };
 
 const options = {
-    params: {    
-    maxResults: '50'    
+  params: {
+    maxResults: '50'
   },
   headers: {
     'X-RapidAPI-Key': process.env.REACT_APP_RAPID_API_KEY,
-    'X-RapidAPI-Host': 'youtube-v31.p.rapidapi.com'
+    'X-RapidAPI-Host': 'youtube-v3-lite.p.rapidapi.com'
   }
 };
 
 
-  export const fetchFromAPI = async (url) => {
-   const {data} =  await axios.get(`${Base_URL}/${url}`, options);
+export const fetchFromAPI = async (url) => {
+  const {data} =  await axios.get(`${Base_URL}/${url}`, options);
 
-   return data;
+  return data;
   }
